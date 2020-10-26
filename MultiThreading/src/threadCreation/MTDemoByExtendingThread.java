6b48bbb7aca4fbbd2971017063ffe117248c1e4b
Abinash <abinash9318@outlook.com>
2020-10-26 19:40:01 +0530

    MT demo by extending Thread class

---
+++ MultiThreading/src/threadCreation/MTDemoByExtendingThread.java
@@ -0,0 +1,23 @@
+package threadCreation;
+
+public class MTDemoByExtendingThread extends Thread{
+	
+	public void run() {
+		try {
+			// Thread.currentThread() returns the ID or name of current running 
+			// thread.
+			System.out.println("Thread " + Thread.currentThread().getId() 
+					           + " is running");
+		} catch(Exception e) {
+			System.out.println("Thread exception occured.");
+		}
+	}
+	
+	public static void main(String...args) {
+		
+		for(int i=0;i<10;i++) {
+			MTDemoByExtendingThread thread = new MTDemoByExtendingThread();
+			thread.start();
+		}
+	}
+}
